5a73c0eb178763b0b0fdbdf2b1ef45d7ae1c9446
zuliz95 <cholieztzuliz@gmail.com>
2022-07-10 07:12:58 +0700

    move docksEventHook to docks since its deprecated on xmonad 0.17.0

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -17,7 +17,7 @@ import qualified Data.Map as M
 
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
+import XMonad.Hooks.ManageDocks (avoidStruts, docks, manageDocks, ToggleStruts(..))
 import XMonad.Hooks.ManageHelpers (isInProperty, isDialog, isFullscreen, doFullFloat, doCenterFloat)
 import XMonad.Hooks.SetWMName
 
@@ -71,34 +71,34 @@ myStartupHook = do
 myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces [1..]
 
 clickable ws = "<action=xdotool key super+"++show i++">"++ws++"</action>"
-	where i = fromJust $ M.lookup ws myWorkspaceIndices
+    where i = fromJust $ M.lookup ws myWorkspaceIndices
 
 windowCount :: X (Maybe String)
 windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
 myShowWNameTheme :: SWNConfig
-myShowWNameTheme = def
-	{ swn_font      = "xft:Ubuntu:bold:size=60"
-	, swn_fade	    = 0.5
-	, swn_bgcolor	= "#1c1f24"
-	, swn_color	    = "#ffffff"
-	}
+myShowWNameTheme = def {
+    swn_font = "xft:Ubuntu:bold:size=60"
+    , swn_fade = 0.5
+    , swn_bgcolor = "#1c1f24"
+    , swn_color = "#ffffff"
+}
 
 mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
 tall = renamed [Replace "tall"]
     $ withBorder myBorderWidth
-	$ smartBorders
-	$ subLayout [] (smartBorders Simplest)
-	$ limitWindows 12
-	$ mySpacing 2
-	$ ResizableTall 1 (3/100) (1/2) []
+    $ smartBorders
+    $ subLayout [] (smartBorders Simplest)
+    $ limitWindows 12
+    $ mySpacing 2
+    $ ResizableTall 1 (3/100) (1/2) []
 
 floats = renamed [Replace "floats"]
     $ withBorder myBorderWidth
     $ smartBorders
-	$ limitWindows 20 
+    $ limitWindows 20 
     $ simplestFloat
 
 wide = renamed [Replace "wide"]
@@ -107,15 +107,15 @@ wide = renamed [Replace "wide"]
     $ subLayout [] (smartBorders Simplest)
     $ limitWindows 12
     $ mySpacing 1
-	$ Mirror Accordion
+    $ Mirror Accordion
 
 myLayoutHook = showWName' myShowWNameTheme 
-	$ avoidStruts
-	$ mouseResize 
-	$ windowArrange
-	$ T.toggleLayouts floats 
-	$ T.toggleLayouts wide
-	$ mkToggle (NBFULL ?? NOBORDERS ?? EOT)
+    $ avoidStruts
+    $ mouseResize 
+    $ windowArrange
+    $ T.toggleLayouts floats 
+    $ T.toggleLayouts wide
+    $ mkToggle (NBFULL ?? NOBORDERS ?? EOT)
     $ tall ||| floats ||| wide
 
 myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
@@ -130,89 +130,90 @@ myManageHook = composeAll [
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X())
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $ [
     ((modm, xK_Return), spawn $ XMonad.terminal conf)
-	, ((modm .|. shiftMask, xK_r), spawn "xmonad --restart")
-	, ((modm .|. shiftMask, xK_q), io exitSuccess)
-	, ((modm, xK_p), spawn "rofi -show drun -show-icons")
+    , ((modm .|. shiftMask, xK_r), spawn "xmonad --restart")
+    , ((modm .|. shiftMask, xK_q), io exitSuccess)
+    , ((modm, xK_p), spawn "rofi -show drun -show-icons")
     , ((modm, xK_e), spawn "rofi -show emoji -modi emoji")
-	, ((modm .|. shiftMask, xK_c), kill1)
-	, ((modm .|. shiftMask, xK_a), killAll)
-	, ((modm, xK_f), sendMessage (T.Toggle "floats"))
-	, ((modm, xK_w), sendMessage (T.Toggle "wide"))
+    , ((modm .|. shiftMask, xK_c), kill1)
+    , ((modm .|. shiftMask, xK_a), killAll)
+    , ((modm, xK_f), sendMessage (T.Toggle "floats"))
+    , ((modm, xK_w), sendMessage (T.Toggle "wide"))
     , ((modm, xK_t), withFocused $ windows . W.sink)
     , ((modm .|. shiftMask, xK_t), sinkAll)
-	, ((modm, xK_m), windows W.focusMaster)
-	, ((modm, xK_j), windows W.focusDown)
-	, ((modm, xK_k), windows W.focusUp)
-	, ((modm .|. shiftMask, xK_j), windows W.swapDown)
-	, ((modm .|. shiftMask, xK_k), windows W.swapUp)
-	, ((modm, xK_h), sendMessage Shrink)
-	, ((modm, xK_l), sendMessage Expand)
-	, ((modm, xK_minus), prevWS)
-	, ((modm, xK_equal), nextWS)
-	, ((modm .|. shiftMask, xK_minus), shiftToPrev)
-	, ((modm .|. shiftMask, xK_equal), shiftToNext)
+    , ((modm, xK_m), windows W.focusMaster)
+    , ((modm, xK_j), windows W.focusDown)
+    , ((modm, xK_k), windows W.focusUp)
+    , ((modm .|. shiftMask, xK_j), windows W.swapDown)
+    , ((modm .|. shiftMask, xK_k), windows W.swapUp)
+    , ((modm, xK_h), sendMessage Shrink)
+    , ((modm, xK_l), sendMessage Expand)
+    , ((modm, xK_minus), prevWS)
+    , ((modm, xK_equal), nextWS)
+    , ((modm .|. shiftMask, xK_minus), shiftToPrev)
+    , ((modm .|. shiftMask, xK_equal), shiftToNext)
     , ((modm, xK_bracketleft), decWindowSpacing 1)
     , ((modm, xK_bracketright), incWindowSpacing 1)
-	, ((modm, xK_space), sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts)
-	, ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
-	, ((modm, xK_Print), spawn "scrot ~/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png")
-	, ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute 0 toggle")
-	, ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume 0 -5%")
-	, ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume 0 +5%")
-	, ((0, xF86XK_MonBrightnessUp), spawn "brightnessctl s +2%")
-	, ((0, xF86XK_MonBrightnessDown), spawn "brightnessctl s 2-%")
-	] ++ [((m .|. modm, k), windows $ f i)
-		| (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-		, (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    , ((modm, xK_space), sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts)
+    , ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
+    , ((modm, xK_Print), spawn "scrot ~/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png")
+    , ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute 0 toggle")
+    , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume 0 -5%")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume 0 +5%")
+    , ((0, xF86XK_MonBrightnessUp), spawn "brightnessctl s +2%")
+    , ((0, xF86XK_MonBrightnessDown), spawn "brightnessctl s 2-%")
+    ] ++ [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
     ] ++ [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_F1, xK_F2, xK_F3] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $ [
     ((modm, button1) ,(\w -> focus w
-		>> mouseMoveWindow w
-		>> ifClick (snapMagicMove (Just 50) (Just 50) w)
-		>> windows W.shiftMaster))
-	, ((modm .|. shiftMask, button1), (\w -> focus w
-		>> mouseMoveWindow w
-		>> ifClick (snapMagicResize [L,R,U,D] (Just 50) (Just 50) w)
-		>> windows W.shiftMaster))
-	, ((modm, button3), (\w -> focus w
-		>> mouseResizeWindow w
-		>> ifClick (snapMagicResize [R,D] (Just 50) (Just 50) w)
-		>> windows W.shiftMaster))
-	, ((modm .|. shiftMask, button3), (\w -> focus w
-		>> Sqr.mouseResizeWindow w True
-		>> ifClick (snapMagicResize [R,D] (Just 50) (Just 50) w)
-		>> windows W.shiftMaster ))
-	]
+        >> mouseMoveWindow w
+        >> ifClick (snapMagicMove (Just 50) (Just 50) w)
+        >> windows W.shiftMaster))
+    , ((modm .|. shiftMask, button1), (\w -> focus w
+        >> mouseMoveWindow w
+        >> ifClick (snapMagicResize [L,R,U,D] (Just 50) (Just 50) w)
+        >> windows W.shiftMaster))
+    , ((modm, button3), (\w -> focus w
+        >> mouseResizeWindow w
+        >> ifClick (snapMagicResize [R,D] (Just 50) (Just 50) w)
+        >> windows W.shiftMaster))
+    , ((modm .|. shiftMask, button3), (\w -> focus w
+        >> Sqr.mouseResizeWindow w True
+        >> ifClick (snapMagicResize [R,D] (Just 50) (Just 50) w)
+        >> windows W.shiftMaster ))
+    ]
 
 main :: IO()
 main = do
-	xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar/xmobarrc"
-	xmonad $ ewmh def {
-		manageHook		        = myManageHook
-		, handleEventHook	    = docksEventHook
-		, modMask		        = myModMask
-		, terminal		        = myTerminal
-		, startupHook		    = myStartupHook
-		, layoutHook		    = myLayoutHook
-		, workspaces		    = myWorkspaces
-		, borderWidth		    = myBorderWidth
-		, normalBorderColor 	= myNormColor
-		, focusedBorderColor	= myFocusColor
-		, keys      			= myKeys
-		, mouseBindings 		= myMouseBindings
-		, logHook		        = dynamicLogWithPP $ xmobarPP {
-			ppOutput = hPutStrLn xmproc
-			, ppCurrent = xmobarColor "#98be65" "" . wrap "[" "]"
-			, ppVisible = xmobarColor "#c792ea" "" . clickable
-			, ppHidden = xmobarColor "#ecbe7b" "" . clickable
-			, ppHiddenNoWindows = xmobarColor "#82AAFF" "" . clickable
-			, ppTitle = xmobarColor "#d0d0d0" "" . shorten 65
-			, ppSep =  "<fc=#666666> <fn=1>|</fn> </fc>"
-			, ppUrgent = xmobarColor "#C45500" "" . wrap "!" "!"
-			, ppExtras  = [windowCount]
-			, ppOrder  = \(w:l:t:e) -> [w,concat([l, ": "]++e),t]
-		}
-	}
+    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar/xmobarrc"
+    xmonad $ docks $ ewmh def {
+        manageHook = myManageHook
+        , modMask = myModMask
+        , terminal = myTerminal
+        , startupHook = myStartupHook
+        , layoutHook = myLayoutHook
+        , workspaces = myWorkspaces
+        , borderWidth = myBorderWidth
+        , normalBorderColor = myNormColor
+        , focusedBorderColor = myFocusColor
+        , keys = myKeys
+        , mouseBindings = myMouseBindings
+        , logHook = dynamicLogWithPP $ xmobarPP {
+            ppOutput = hPutStrLn xmproc
+            , ppCurrent = xmobarColor "#98be65" "" . wrap "[" "]"
+            , ppVisible = xmobarColor "#c792ea" "" . clickable
+            , ppHidden = xmobarColor "#ecbe7b" "" . clickable
+            , ppHiddenNoWindows = xmobarColor "#82AAFF" "" . clickable
+            , ppTitle = xmobarColor "#d0d0d0" "" . shorten 65
+            , ppSep =  "<fc=#666666> <fn=1>|</fn> </fc>"
+            , ppUrgent = xmobarColor "#C45500" "" . wrap "!" "!"
+            , ppExtras  = [windowCount]
+            , ppOrder  = \(w:l:t:e) -> [w,concat([l, ": "]++e),t]
+       }
+    }
+
+
